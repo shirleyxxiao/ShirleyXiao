ebfdb5ff3ff2ba9834c25b36bf5412de79c78465
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-27 12:58:22 -0400

    testing

---
+++ mail-template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <meta charset="utf-8">
+   <title>HTML email template</title>
+   <style>
+    </head>
+    <body>
+    <div class="page">
+        <h1>Thank you <?php echo @$name ?> </h1>
+    
+        <!-- More content here -->
+    
+        <footer>
+            <p>MESSAGE</p>
+        </footer>
+    </div>
+    </body>
+</html>
