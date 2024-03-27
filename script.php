643192e49961a5ba70967e25f3aceb1e5b2f3ce3
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-27 12:43:39 -0400

    email

--- script.php
+++ script.php
@@ -6,5 +6,8 @@ $subject = "message from shirleyxiao.co"
 //array headers
 
 $headers array(
-    
+    "MIME-Version" => "1.0",
+    "Content-Type" => "text/html;charset=UTF-8",
+    "From" => "shirleyxiaocoop@gmail.com",
+    "Reply-To"
 )
