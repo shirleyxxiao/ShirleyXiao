ac9353c1956c872ee9987961f6c3a704b0507e18
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-27 12:45:45 -0400

    more email

--- script.php
+++ script.php
@@ -8,6 +8,9 @@ $subject = "message from shirleyxiao.co"
 $headers array(
     "MIME-Version" => "1.0",
     "Content-Type" => "text/html;charset=UTF-8",
-    "From" => "shirleyxiaocoop@gmail.com",
-    "Reply-To"
+    "From" => "shirleyxiaocoop@gmail.com", //should be the user email
+    "Reply-To" => "shirleyxiaocoop@gmail.com"
 )
+
+//will get data from the message box in GetInTouch.html page
+$message
