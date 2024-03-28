fa0b4787843e4ca9f689b2ad7979ea07eb627c59
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-28 18:52:44 -0400

    script

--- script.php
+++ script.php
@@ -3,20 +3,17 @@
 $to = "shirleyxiaocoop@gmail.com";
 $subject = "message from shirleyxiao.co";
 
-//array headers
+// array headers
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Type: text/html;" . "\r\n";
+$headers .= "From: shirleyxiaocoop@gmail.com" . "\r\n"; // should be the user email
+$headers .= "Reply-To: shirleyxiaocoop@gmail.com" . "\r\n";
 
-$headers = array(
-    "MIME-Version" => "1.0",
-    "Content-Type" => "text/html;charset=UTF-8",
-    "From" => "shirleyxiaocoop@gmail.com", //should be the user email
-    "Reply-To" => "shirleyxiaocoop@gmail.com"
-);
-
-//will get data from the message box in GetInTouch.html page
+// will get data from the message box in GetInTouch.html page
 $message = file_get_contents("mail-template.php");
 
 $send = mail($to, $subject, $message, $headers);
 
-//checking if email sent
-
+// checking if email sent
 echo ($send ? "Mail has successfully sent" : "Error!");
+?>
