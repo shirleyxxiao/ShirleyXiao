b0679ee4c8b875b1e3b19cee34e9ae2c69d66d3e
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-31 11:14:28 -0400

    edit to script

--- mail-template.php
+++ mail-template.php
@@ -23,29 +23,3 @@
 </body>
 </html>
 
-
-<!--
-<!DOCTYPE html>
-
-<html>
-    <head>
-    <meta charset="utf-8">
-   <title>HTML email template</title>
-   <style>
-    </head>
-    <body>
-    <div class="page">
-        <h1>Thank you <?php echo @$name ?> </h1>
-    
- More content here 
-    
-        <footer>
-            <p>MESSAGE</p>
-        </footer>
-    </div>
-    </body>
-</html>
--->
-
-
-
