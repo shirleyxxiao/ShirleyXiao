65158e2aa1e2f9c982155054502e5df2d9969feb
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-03-31 11:13:07 -0400

    new  scripts

--- mail-template.php
+++ mail-template.php
@@ -1,4 +1,31 @@
 <!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>HTML email template</title>
+   <style>
+      <!-- The css code goes here -->
+   </style>
+</head>
+<body>
+   <div class="page">
+      <h1>Thank you for your purchase <?php echo @$name ?> </h1>
+ 
+      <!-- More content here -->
+ 
+      <footer>
+         <a href="https://google.com">Hyperlink</a> |
+         <a href="https://digitalfox-tutoroals.com">Hyperlink</a> |
+         <a href="">Hyperlink</a> |
+         <a href="">Hyperlink</a>
+      </footer>
+   </div>
+</body>
+</html>
+
+
+<!--
+<!DOCTYPE html>
 
 <html>
     <head>
@@ -10,7 +37,7 @@
     <div class="page">
         <h1>Thank you <?php echo @$name ?> </h1>
     
-        <!-- More content here -->
+ More content here 
     
         <footer>
             <p>MESSAGE</p>
@@ -18,3 +45,7 @@
     </div>
     </body>
 </html>
+-->
+
+
+
--- script.php
+++ script.php
@@ -1,19 +1,29 @@
+
 <?php
-
-$to = "shirleyxiaocoop@gmail.com";
-$subject = "message from shirleyxiao.co";
-
-// array headers
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-Type: text/html;" . "\r\n";
-$headers .= "From: shirleyxiaocoop@gmail.com" . "\r\n"; // should be the user email
-$headers .= "Reply-To: shirleyxiaocoop@gmail.com" . "\r\n";
-
-// will get data from the message box in GetInTouch.html page
-$message = file_get_contents("mail-template.php");
-
-$send = mail($to, $subject, $message, $headers);
-
-// checking if email sent
-echo ($send ? "Mail has successfully sent" : "Error!");
-?>
+   $to = "someone@mail.com";
+   $subject = "Thank you for your purchase";
+ 
+   // In php 7.2 and newer versions we can use an array to set the headers.
+   $headers = array(
+    	'MIME-Version' => '1.0',
+    	'Content-type' => 'text/html;charset=UTF-8',
+    	'From' => 'dennis@mail.com',
+    	'Reply-To' => 'dennis@mail.com'
+   );
+ 
+   // Setting the value in the $name variable.
+   $name = "Dennis";
+ 
+   // Starting output buffer.
+   ob_start();
+   include("mail-template.php");
+   $message = ob_get_contents();
+   ob_end_clean();
+ 
+   $sent = mail($to, $subject, $message, $headers);
+ 
+   if(!$sent){
+      echo "Error: Message not sent. Please try again";
+   }else{
+      echo "Message was sent successfully";
+   }
