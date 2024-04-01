a374c364ccceac141c3438a1e5b374813e7ba5c7
shirleyxxiao <143335350+shirleyxxiao@users.noreply.github.com>
2024-04-01 14:34:20 -0400

    updates

--- mail-template.php
+++ mail-template.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
+
 <html>
 <head>
    <meta charset="utf-8">
    <title>HTML email template</title>
    <style>
       <!-- The css code goes here -->
-   </style>
+    </style>
 </head>
 <body>
    <div class="page">
       <h1>Thanks! <?php echo @$name ?> </h1>
  
       <!-- More content here -->
- 
-      <footer>
 
-      </footer>
+
    </div>
 </body>
 </html>
